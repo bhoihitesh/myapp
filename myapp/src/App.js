94a90867647d4acdf1d7c4ce6c3b5a882835023c
bhoihitesh <bhoihitesh183@gmail.com>
2022-11-05 06:36:15 +0530

    add a new line

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Provider } from "react-redux";
+import { Provider } from "react-redux"; 
+
 import { PersistGate } from 'redux-persist/integration/react'
 import { Store } from "./services/redux/Store";
 import { persistStore } from "redux-persist";
